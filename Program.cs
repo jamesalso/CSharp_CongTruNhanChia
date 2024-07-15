094b7d61d7dfc86705afd1debdc98a1ef2ef2441
jamesalso <minhkhuong160899@gmail.com>
2024-07-15 20:29:28 +0700

    Add function

--- Program.cs
+++ Program.cs
@@ -6,29 +6,42 @@
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.WriteLine("Hello Word!");
         Console.WriteLine("Chuong Trinh Tong 2 So");
-        int so1, so2, tong, hieu, tich;
+        int so1, so2;
         Console.Write("Nhap so 1: ");
         so1 = Convert.ToInt32(Console.ReadLine());
         Console.Write("Nhap so 2: ");
         so2 = Convert.ToInt32(Console.ReadLine());
 
-        tong = so1 + so2;
-        Console.WriteLine("Tong cua 2 so " + so1 + " và " + so2 + " la: " + tong);
+        Console.WriteLine("Tong cua 2 so " + so1 + " và " + so2 + " la: " + tinhTong(so1, so2));
+        Console.WriteLine("Hieu cua 2 so " + so1 + " và " + so2 + " la: " + tinhHieu(so1, so2));
+        Console.WriteLine("Tich cua 2 so " + so1 + " và " + so2 + " la: " + tinhTich(so1, so2));
 
-        hieu = so1 - so2;
-        Console.WriteLine("Hieu cua 2 so " + so1 + " và " + so2 + " la: " + hieu);
-
-        tich = so1 * so2;
-        Console.WriteLine("Tich cua 2 so " + so1 + " và " + so2 + " la: " + tich);
-
-        double thuong;
         try
         {
-            thuong = so1 / so2;
-            Console.WriteLine("Thuong cua 2 so " + so1 + " và " + so2 + " la: " + thuong);
+            Console.WriteLine("Thuong cua 2 so " + so1 + " và " + so2 + " la: " + tinhThuong(so1, so2));
         }
         catch {
             Console.WriteLine("Khong the chia cho 0");
         }
+    }
+
+    private static int tinhTong(int a, int b)
+    {
+        return a + b;
+    }
+
+    private static int tinhHieu(int a, int b)
+    {
+        return a - b;
+    }
+
+    private static int tinhTich(int a, int b)
+    {
+        return a * b;
+    }
+
+    private static double tinhThuong(double a, double b)
+    {
+        return a / b;
     }
 }
